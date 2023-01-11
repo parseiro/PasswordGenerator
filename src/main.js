bb7922ee7cf0e6b7b96f90c6aa4b95817025f8ef
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-01-10 22:12:24 -0300

    Copy to clipboard

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import {nanoid} from 'nanoid';
+import copy from 'clipboard-copy';
 import './main.css';
 
 const passwordBtnEl = document.getElementById('passwordBtn');
 const displayPasswordEl = document.getElementById('passwordEl');
 
 passwordBtnEl.addEventListener('click', () => {
-    displayPasswordEl.textContent = nanoid();
+    const password = nanoid();
+    displayPasswordEl.textContent = password;
+    copy(password);
+    alert('Copiado para a área de transferência!');
 });
